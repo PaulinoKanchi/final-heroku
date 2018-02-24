b454eabd432e2cfcfa3cb4a5d1c06e7f973d6909
T-107 <T-107@PC185>
2018-02-23 21:04:19 -0600

    4

--- src/main/java/org/unitec/finalheroku/ControladorMensajes.java
+++ src/main/java/org/unitec/finalheroku/ControladorMensajes.java
@@ -7,31 +7,62 @@ package org.unitec.finalheroku;
 
 import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 
 public class ControladorMensajes {
 
     @Autowired
     RepositorioMensajito repoMensa;
+    //buscar todos
 
+    @CrossOrigin
     @RequestMapping(value = "/mensajitos", method = RequestMethod.GET,
             headers = {"Accept=application/json"})
-
     public ArrayList<Mensajitos> hola() {
-
         return (ArrayList<Mensajitos>) repoMensa.findAll();
     }
 
-    @RequestMapping(value = "/mensajitos/{id}/{titulo}/{cuerpo}", method = RequestMethod.GET,
-            headers = {"Accept=application/json"})
-    public Estatus guardar(@PathVariable String id,@PathVariable String titulo, @PathVariable String cuerpo) {
-        repoMensa.save(new Mensajitos(id,titulo, cuerpo));
+    //buscar por id
+    @CrossOrigin
+    @RequestMapping(value = "/mensajitos/{id}", method = RequestMethod.GET,
+    headers = {"Accept=application/json"})
+    public Mensajitos hola(@PathVariable String id) {
+        return repoMensa.findOne(id);
+    }
+
+    //actualizar 
+    @CrossOrigin
+    @RequestMapping(value = "/mensajitos/{id}/{titulo}/{cuerpo}", method = RequestMethod.PUT,
+    headers = {"Accept=application/json"})
+    public Estatus actualizar(@PathVariable String id, @PathVariable String titulo, @PathVariable String cuerpo) {
+        repoMensa.save(new Mensajitos(id, titulo, cuerpo));
+        return new Estatus(true, "Actualizado con exito");
+    }
+
+    //Guardar
+    @CrossOrigin
+    @RequestMapping(value = "/mensajitos//{titulo}/{cuerpo}", method = RequestMethod.POST,
+    headers = {"Accept=application/json"})
+    public Estatus guardar(@PathVariable String titulo, @PathVariable String cuerpo) {
+        repoMensa.save(new Mensajitos(titulo, cuerpo));
         return new Estatus(true, "Guardado con exito");
+    }
+
+    //Borrar
+    @CrossOrigin
+    @RequestMapping(value = "/mensajito/{id}", method = RequestMethod.DELETE,
+    headers = {"Accept=application/json"})
+    public Estatus borrarMensaje(@PathVariable String id) {
+        Estatus estatus = new Estatus(true, "Borrado con exito");
+        repoMensa.delete(new Mensajitos(id));
+        return estatus;
     }
 }
--- src/main/java/org/unitec/finalheroku/RepositorioMensajito.java
+++ src/main/java/org/unitec/finalheroku/RepositorioMensajito.java
@@ -2,7 +2,7 @@ package org.unitec.finalheroku;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface RepositorioMensajito extends MongoRepository<Mensajitos,Integer >{
+public interface RepositorioMensajito extends MongoRepository<Mensajitos,String>{
     
     
     
